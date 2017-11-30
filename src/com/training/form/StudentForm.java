f0a9e1f7366d9c0fe2581781f560490a9917184e
yuanjie92 <425697230@qq.com>
2017-11-30 16:24:07 +0800

    测试springmvc环境

---
+++ src/com/training/form/StudentForm.java
@@ -0,0 +1,42 @@
+package com.training.form;
+
+public class StudentForm {
+
+	private Integer id;
+	private String name;
+	private String clazz;
+	private String birthday;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+}
